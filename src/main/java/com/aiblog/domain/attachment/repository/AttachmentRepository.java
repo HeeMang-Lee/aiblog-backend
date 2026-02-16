44d8237a4b6b5d83e6dcf28d85faab6ff23f0fae
hemsej018 <hemsej018@naver.com>
2026-02-16 23:18:01 +0900

    [feat] AttachmentRepository 추가

---
+++ src/main/java/com/aiblog/domain/attachment/repository/AttachmentRepository.java
@@ -0,0 +1,10 @@
+package com.aiblog.domain.attachment.repository;
+
+import com.aiblog.domain.attachment.entity.Attachment;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
+
+  List<Attachment> findByPostIdOrderByDisplayOrder(Long postId);
+}
